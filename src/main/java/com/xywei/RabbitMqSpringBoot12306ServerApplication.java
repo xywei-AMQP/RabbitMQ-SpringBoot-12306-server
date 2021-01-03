11e08c89374875742b2c571a743fabbe529a3e19
winfuture <insoul_net@qq.com>
2021-01-03 15:53:05 +0800

    初始化工程

---
+++ src/main/java/com/xywei/RabbitMqSpringBoot12306ServerApplication.java
@@ -0,0 +1,13 @@
+package com.xywei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqSpringBoot12306ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqSpringBoot12306ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xywei/RabbitMqSpringBoot12306ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xywei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqSpringBoot12306ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
